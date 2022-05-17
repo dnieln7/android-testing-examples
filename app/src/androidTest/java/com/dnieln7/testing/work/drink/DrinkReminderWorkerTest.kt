b7392b8866f284c661dedbdcd459b1459767dcb2
Daniel Nolasco <daniel.nolasco@rookmotion.com>
2022-05-16 19:12:38 -0500

    Added ListenableWorker unit test

--- app/src/androidTest/java/com/dnieln7/testing/work/drink/DrinkReminderWorkerTest.kt
+++ app/src/androidTest/java/com/dnieln7/testing/work/drink/DrinkReminderWorkerTest.kt
@@ -6,12 +6,14 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.work.*
 import androidx.work.impl.utils.SynchronousExecutor
+import androidx.work.testing.TestListenableWorkerBuilder
 import androidx.work.testing.WorkManagerTestInitHelper
 import com.dnieln7.testing.work.drink.workers.DrinkReminderWorker
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class DrinkReminderWorkerTest {
@@ -54,6 +56,7 @@ class DrinkReminderWorkerTest {
         val workManager = WorkManager.getInstance(context)
 
         workManager.enqueue(drinkRequest).result.get()
+
         testDriver?.setAllConstraintsMet(drinkRequest.id)
 
         val workInfo = workManager.getWorkInfoById(drinkRequest.id).get()
@@ -72,7 +75,7 @@ class DrinkReminderWorkerTest {
 
         val drinkData = workDataOf("NAME" to "TEST")
 
-        val drinkRequest = OneTimeWorkRequestBuilder<DrinkReminderWorker>()
+        val drinkRequest = PeriodicWorkRequestBuilder<DrinkReminderWorker>(30, TimeUnit.MINUTES)
             .addTag(tag)
             .setInputData(drinkData)
             .setConstraints(constraints)
@@ -81,11 +84,23 @@ class DrinkReminderWorkerTest {
         val workManager = WorkManager.getInstance(context)
 
         workManager.enqueue(drinkRequest).result.get()
+
         testDriver?.setInitialDelayMet(drinkRequest.id)
         testDriver?.setAllConstraintsMet(drinkRequest.id)
+        testDriver?.setPeriodDelayMet(drinkRequest.id)
+    }
 
-        val workInfo = workManager.getWorkInfoById(drinkRequest.id).get()
+    @Test
+    fun listenable() {
+        val drinkData = workDataOf("NAME" to "TEST")
 
-        assertEquals(workInfo.state, WorkInfo.State.SUCCEEDED)
+        val worker = TestListenableWorkerBuilder<DrinkReminderWorker>(context)
+            .setTags(listOf(tag))
+            .setInputData(drinkData)
+            .build()
+
+        val result = worker.startWork().get()
+
+        assertEquals(result, ListenableWorker.Result.success())
     }
 }
